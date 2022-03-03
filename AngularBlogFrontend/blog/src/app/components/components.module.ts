f0f161e482e94b71993129b44c97f8780bd27336
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-03 19:07:19 +0300

    sayfalama butonları için ngx-pagination kütüphanesinin projeye eklenmesi

--- AngularBlogFrontend/blog/src/app/components/components.module.ts
+++ AngularBlogFrontend/blog/src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
 import { RouterModule } from '@angular/router';
 
@@ -7,7 +8,7 @@ import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
   declarations: [MenuCategoryComponent, PageTitleComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [MenuCategoryComponent,PageTitleComponent],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
+  exports: [MenuCategoryComponent, PageTitleComponent],
 })
 export class ComponentsModule {}
