03e8a87f89fc8f9f1a0fbab5d30054a07181a236
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-03 15:34:28 +0300

    Main-layout'un içerisinde yer alacak sayfalarımız için modul oluşturma

--- AngularBlogFrontend/blog/src/app/app-routing.module.ts
+++ AngularBlogFrontend/blog/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
