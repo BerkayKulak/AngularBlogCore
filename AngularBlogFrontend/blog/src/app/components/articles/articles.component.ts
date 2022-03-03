365025035aacabe73cd829b95c07a80058c26ff5
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-03 23:38:56 +0300

    Makalelerin yüklenmeleri sırasında content loading placeholder kullanımı

--- AngularBlogFrontend/blog/src/app/components/articles/articles.component.ts
+++ AngularBlogFrontend/blog/src/app/components/articles/articles.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/models/article';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-articles',
@@ -12,13 +13,27 @@ export class ArticlesComponent implements OnInit {
   @Input() articles: Article[];
   @Input() page: number;
   @Input() pageSize: number;
+  @Input() loadingItem: number;
 
   default_article: string = 'assets/article_empty.png';
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public articleService: ArticleService
+  ) {}
+
+  createRange() {
+    var items: number[] = [];
+    for (var i = 1; i <= this.loadingItem; i++) {
+      items.push(i);
+    }
+    return items;
+  }
 
   ngOnInit(): void {}
 
   pageChanged(event) {
+    this.articleService.loading = true;
     this.page = event;
     this.router.navigateByUrl(`/sayfa/${this.page}`);
   }
