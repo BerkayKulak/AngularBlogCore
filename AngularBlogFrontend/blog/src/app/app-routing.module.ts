6a303b43cf13ed1823091a2ff694d80b0f7f562e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-04 15:39:36 +0300

    Archive Component'inin oluşturulması(arşiv sayfası için)-2

--- AngularBlogFrontend/blog/src/app/app-routing.module.ts
+++ AngularBlogFrontend/blog/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { CategoryArticlesComponent } from './pages/category-articles/category-articles.component';
 import { SearchComponent } from './pages/search/search.component';
+import { ArchiveComponent } from './pages/archive/archive.component';
 
 const routes: Routes = [
   {
@@ -25,20 +26,28 @@ const routes: Routes = [
         component: HomeComponent,
       },
       {
-        path:'makale/:title/:id',
-        component:ArticleComponent
+        path: 'makale/:title/:id',
+        component: ArticleComponent,
       },
       {
-        path:'kategori/:name/:id',
-        component:CategoryArticlesComponent
+        path: 'kategori/:name/:id',
+        component: CategoryArticlesComponent,
       },
       {
-        path:'kategori/:name/:id/sayfa/:page',
-        component:CategoryArticlesComponent
+        path: 'kategori/:name/:id/sayfa/:page',
+        component: CategoryArticlesComponent,
       },
       {
-        path:"arama/sayfa/:page",
-        component:SearchComponent
+        path: 'arama/sayfa/:page',
+        component: SearchComponent,
+      },
+      {
+        path: 'arsiv/:year/:month',
+        component: ArchiveComponent,
+      },
+      {
+        path: 'arsiv/:year/:month/sayfa/:page',
+        component: ArchiveComponent,
       },
       {
         path: 'hakkimda',
